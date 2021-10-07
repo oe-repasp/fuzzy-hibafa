47912f4cce0892b24f5f9b8de6d9f39d8ec8d727
oe-repasp <repasp@repasp.hu>
2021-10-07 20:23:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,11 @@
 #if __name__ == '__main__':
 #    print_hi('PyCharm')
 print("1")
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, linestyle = 'dotted', color = 'r')
+plt.show()
