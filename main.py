bce49a54f1602abe57d65c4c9b2eb134bf1a652d
oe-repasp <repasp@repasp.hu>
2021-10-07 20:31:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ print("1")
 import matplotlib.pyplot as plt
 import numpy as np
 
-ypoints = np.array([3, 8, 1, 10])
+ypoints = np.array([0, 1, 1, 0])
 
 plt.plot(ypoints, linestyle = 'dotted', color = 'r')
+
+
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(xpoints, ypoints)
 plt.show()
