6681ff42e4baa15dc346ada12947b4fb32305d22
oe-repasp <repasp@repasp.hu>
2021-10-09 22:57:39 +0200

    sql try

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from app.db import conn
+import mariadb
 
 cur=conn.cursor()
 
-cur.execute("select @@hostname")
+my_query="select * from events"
+
+cur.execute(my_query)
+
+records=cur.fetchall()
 
 
-for i in cur:
-    print(i)
+for sor in records:
+    print('{:40}: {:5} {:4} '.format(sor[1],"value",sor[2]))
