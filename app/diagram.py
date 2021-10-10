d2e9422b4df6f93af1b152b03690fbcbd5893f50
oe-repasp <repasp@repasp.hu>
2021-10-10 12:14:17 +0200

    diagram elements change

--- app/diagram.py
+++ app/diagram.py
@@ -1,17 +1,15 @@
 from diagrams import Cluster, Diagram
-from diagrams.aws.compute import ECS
-from diagrams.aws.database import ElastiCache, RDS
-from diagrams.aws.network import ELB
+from diagrams.custom import Custom
 from diagrams.aws.network import Route53
 
 with Diagram("Fuzzy Fault Tree Analysis", show=True):
     level0 = Route53("Main event")
-    level1 = ELB("OR gate")
+    level1 = Custom("and","..\pict\logic_and.png")
 
-    with Cluster("LEVEL 1 Events"):
-        svc_group = [ECS("event1"),
-                     ECS("event2"),
-                     ECS("event3")]
+    # with Cluster("LEVEL 1 Events"):
+    #     svc_group = [ECS("event1"),
+    #                  ECS("event2"),
+    #                  ECS("event3")]
     #
     # with Cluster("DB Cluster"):
     #     db_main = RDS("userdb")
