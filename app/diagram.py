b9839ef7f9dc182bd39441c1720efeff91cf75b8
oe-repasp <repasp@repasp.hu>
2021-10-10 12:53:58 +0200

    diagram

--- app/diagram.py
+++ app/diagram.py
@@ -1,19 +1,25 @@
+from typing import List
+
 from diagrams import Cluster, Diagram
 from diagrams.custom import Custom
-from diagrams.aws.network import Route53
+
 
 with Diagram("Fuzzy Fault Tree Analysis", show=True):
-    level0 = Route53("Main event")
-    level1 = Custom("and","..\pict\logic_and.png")
+    level0 = Custom("Main event","..\pict\main_event.png")
+    level1gate = Custom("AND","..\pict\logic_and.png")
+    level2gate = Custom("OR","..\pict\logic-or.png")
 
-    # with Cluster("LEVEL 1 Events"):
-    #     svc_group = [ECS("event1"),
-    #                  ECS("event2"),
-    #                  ECS("event3")]
-    #
-    # with Cluster("DB Cluster"):
-    #     db_main = RDS("userdb")
-    #     db_main - [RDS("userdb ro")]
+    with Cluster("LEVEL 1 Events"):
+        level1 = [
+            Custom("Event 1","..\pict\event_1.png"),
+            Custom("Event 2","..\pict\event_2.png"),
+            Custom("Event 3","..\pict\event_3.png")]
 
-    level1 >> level0
-    svc_group >> level1
+    with Cluster("LEVEL 2 Events"):
+        level2 = [
+            Custom("Event 4","..\pict\event_4.png"),
+            Custom("Event 5","..\pict\event_5.png")]
+
+    level2 >> level2gate >> level1 >> level1gate >> level0
+
+
